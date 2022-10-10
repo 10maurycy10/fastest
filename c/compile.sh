012bf515cdb7bd245a9e637497c5381a78f3945f
RaidTheWeb <therealraidtheweb@gmail.com>
2022-10-11 12:02:00 +1300

    Put C on even footing. (backend wise)

--- c/compile.sh
+++ c/compile.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-gcc prime.c -O2 -o prime_o2 --static
-gcc prime.c -Os -o prime_os --static
-gcc prime.c -o prime_o0 --static
+clang prime.c -O2 -o prime_o2 --static
+clang prime.c -Os -o prime_os --static
+clang prime.c -o prime_o0 --static
